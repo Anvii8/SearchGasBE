eb6fd15305901106b6a242f68ea2a3c268bb9c23
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-11-10 12:43:10 +0100

    funcionalidad login y logueado implementada

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,9 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UserDto } from 'src/users/user.dto';
-import { UsersService } from 'src/users/users.service';
-import { JWTPayload } from './auth.service';
+import { UserDto } from 'src/users/models/user.dto';
+import { UsersService } from 'src/users/services/users.service';
+import { JWTPayload } from '../services/auth.service';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
